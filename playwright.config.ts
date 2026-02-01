ca7494a33b62a0c64e45de9b432f123e4fb97ee8
Slobodan <slobodan.m.zivic@gmail.com>
2026-02-01 02:04:39 +0100

    fixing bugs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
-timeout: 40 * 1000, //40000 ms(30 secs)
+timeout: 30 * 1000, //30000 ms(30 secs)
 testDir: './tests',
 fullyParallel: true,
 //retries: process.env.CI ? 2 : 0,
-retries: 1,
+retries: 0,
 //workers: process.env.CI ? 1 : undefined,
 workers: 1,
 reporter: [
